6c4d1bfa4ec03d50dfc89c5e0ac29e85d9bd7b8a
John Doe <john@sample.com>
2017-03-28 23:50:14 +0300

    first commit

---
+++ ClassLibrary1/CalcTesting.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace CalcTest
+{
+   
+   [TestFixture]
+    public class CalcTest
+    {
+        [Test, Order(1)]
+        public void StartCalc()
+        {
+            //arrange
+            string expected = "calc";
+            //act
+            Process.Start("calc.exe");
+            //assert
+            Assert.NotZero(Process.GetProcessesByName(expected).Length);
+      
+        }
+        [Test, Order(2)]
+        public void PressFive()
+        {
+            //arrange
+            Thread.Sleep(200);
+            string expected = "5";
+            AutomationElement mainCalcWindow = AutomationElement.RootElement.FindFirst(TreeScope.Descendants,
+                new AndCondition(
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window),
+                    new PropertyCondition(AutomationElement.ClassNameProperty, "CalcFrame")));
+            var buttonFive = mainCalcWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "135"));
+            InvokePattern buttonFivePattern = buttonFive.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            //act
+            buttonFivePattern.Invoke();
+            //assert
+            var resultText= mainCalcWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "150"));
+            var actual = resultText.GetCurrentPropertyValue(AutomationElement.NameProperty).ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test, Order(3)]
+        public void CloseCalc()
+        {
+            //arrange
+            string processName = "calc";
+            var processes = Process.GetProcessesByName(processName);
+            if (processes.Length != 0) 
+            {
+                //act
+                processes.Where(p => p.ProcessName.Contains(processName)).ToList().ForEach(pr=>pr.Kill());
+                Thread.Sleep(100);
+                //assert
+                Assert.Zero(Process.GetProcessesByName(processName).Length);
+            }
+        }
+    }
+}
---
+++ WebWithSelenium/SeleniumTesting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebWithSelenium
+{
+    [TestFixture]
+    public class SeleniumTesting
+    {
+        [Test]
+        public void SearchYandex()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://google.com.ua";
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("yandex");
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);
+              var firstLink = driver.FindElement(By.Id("vs0p1c0"));
+            firstLink.Click();
+
+            //validate
+            if (driver.FindElement(By.CssSelector(".logo.logo_color_ru")) == null) Assert.Fail();
+            driver.Quit();
+            //new comment line
+        }
+
+        [Test]
+        public void DoSmthing()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://google.com.ua";
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("wikipedia");
+            searchBox.Submit();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            
+           var enWikiLinkcontainer = wait.Until(gr =>
+            {
+                var links = driver.FindElements(By.ClassName("rc"));
+                return (links.Count > 0)?  links[1] :  null;
+            });
+            var enWikiLink = enWikiLinkcontainer.FindElement(By.TagName("a"));
+            enWikiLink.Click();
+
+            //validate
+            if (driver.FindElement(By.CssSelector(".central-textlogo > img:nth-child(1)")) == null) Assert.Fail();
+            driver.Quit();
+
+        }
+
+    }
+}
